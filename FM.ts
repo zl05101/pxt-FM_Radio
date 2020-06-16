2455b32241663fa3a260bf5c73c92ef7460bfda8
lz05101 <328222051@qq.com>
2020-06-16 14:41:33 +0800

    add interface

--- FM.ts
+++ FM.ts
@@ -55,4 +55,72 @@ namespace RDA5807M{
         reg_data[1] = 0x01;
         write_register();
     }
+
+    /**
+     * FM向下搜台
+     * @param none
+     */
+    //% blockId="RDA5807M_seekDown" block="FM向下搜台"
+    //% weight=70 blockGap=8
+    //% parts=RDA5807M trackArgs=0
+    export function seekDown():void{
+        reg_data[0] &= ~(1 << 1);
+        seek();
+    }
+
+    /**
+     * FM向上搜台
+     * @param none
+     */
+    //% blockId="RDA5807M_seekUp" block="FM向上搜台"
+    //% weight=70 blockGap=8
+    //% parts=RDA5807M trackArgs=0
+    export function seekUp():void{
+        reg_data[0] |= 1 << 1;
+        seek();
+    }
+
+    /**
+     * FM获取频率
+     * @param none
+     */
+    //% blockId="RDA5807M_getFreq" block="FM获取频率"
+    //% weight=70 blockGap=8
+    //% parts=RDA5807M trackArgs=0
+    export function getFreq():number{
+        let temp = (reg_data[2] * 256) + (reg_data[3] & 0xC0);
+        temp = temp >> 6;
+        let freq = 10 * temp + 8700;
+        return freq;
+    }
+
+    /**
+     * FM音量加大
+     * @param none
+     */
+    //% blockId="RDA5807M_volumeUp" block="FM音量加大"
+    //% weight=70 blockGap=8
+    //% parts=RDA5807M trackArgs=0
+    export function volumeUp():void{
+        if((reg_data[7] & 0x0F) < 0xF){
+            reg_data[7]++;
+        }
+        reg_data[0] = 0xD0;
+        write_register();
+    }
+
+    /**
+     * FM音量减小
+     * @param none
+     */
+    //% blockId="RDA5807M_volumeDown" block="FM音量减小"
+    //% weight=70 blockGap=8
+    //% parts=RDA5807M trackArgs=0
+    export function volumeDown():void{
+        if((reg_data[7] & 0x0F) > 0){
+            reg_data[7]--;
+        }
+        reg_data[0] = 0xD0;
+        write_register();
+    }
 }
