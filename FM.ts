491dc0a1d80649f6ad02f77d5dc6d76a184acd31
lz05101 <328222051@qq.com>
2020-06-16 15:03:35 +0800

    fix buf

--- FM.ts
+++ FM.ts
@@ -7,34 +7,41 @@
 //% weight=20 color=#0855AA icon="\uf040" block="心点FM收音机"
 namespace RDA5807M{
     let RDA5807M_SLAVE_ADDRESS = 0x10;
-    let reg_data:number[] = [0xD0, 0x00, 0x00, 0x00, 0x00, 0x40, 0x90, 0x8F];
+    let reg_data = pins.createBuffer(8);
+    let reg_read_data = pins.createBuffer(4);
+    reg_data[0] = 0xd0;
+    reg_data[1] = 0x0;
+    reg_data[2] = 0x0;
+    reg_data[3] = 0x0;
+    reg_data[4] = 0x0;
+    reg_data[5] = 0x40;
+    reg_data[6] = 0x90;
+    reg_data[7] = 0x8F;
 
     function write_register():void{
         pins.i2cWriteBuffer(RDA5807M_SLAVE_ADDRESS, reg_data);
     }
 
     function read_register():void{
-        let buf:number[] = pins.i2cReadBuffer(RDA5807M_SLAVE_ADDRESS, 4);
-        return buf;
+        reg_read_data = pins.i2cReadBuffer(RDA5807M_SLAVE_ADDRESS, 4);
     }
 
     function seek():void{
-        let reg_temp:number[] = [0,0,0,0]
         reg_data[3] &= ~(1 << 4);
         reg_data[0] |= 0x01;
         write_register();
         while(1){
             pause(100);
-            reg_temp = read_register();
-            if((reg_temp[0] & 0x40) == 0x40){
+            read_register();
+            if((reg_read_data[0] & 0x40) == 0x40){
                 break;
             }
         }
-        let chan = reg_temp[0] & 0x03
-        chan = reg_temp[1] | (chan << 8)
-        chan = chan << 6
-        reg_data[2] = (chan >> 8) & 0xff
-        reg_data[3] = (chan & 0xff)
+        let chan = reg_read_data[0] & 0x03;
+        chan = reg_read_data[1] | (chan << 8);
+        chan = chan << 6;
+        reg_data[2] = (chan >> 8) & 0xff;
+        reg_data[3] = (chan & 0xff);
     }
 
 
@@ -46,11 +53,11 @@ namespace RDA5807M{
     //% weight=70 blockGap=8
     //% parts=RDA5807M trackArgs=0
     export function begin():void{
-        sleep_ms(50)
+        pause(50)
         reg_data[0] = 0x00;
         reg_data[1] = 0x02;
         write_register();
-        sleep_ms(50);
+        pause(50);
         reg_data[0] = 0xD0;
         reg_data[1] = 0x01;
         write_register();
